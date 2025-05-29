bc791df79839d651cfe334aaf2dd6f817cf8ff1a
João Sereia <joao.lo.sereia@gmail.com>
2025-05-29 15:19:53 -0300

    chore(analysis_options): add very_good_analysis

--- test/aurasync_ui_test.dart
+++ test/aurasync_ui_test.dart
@@ -1,6 +1,5 @@
-import 'package:flutter_test/flutter_test.dart';
-
 import 'package:aurasync_ui/aurasync_ui.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('adds one to input values', () {
