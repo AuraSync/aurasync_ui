7febeb1ba2f793912a709ac5c7bd7606280f0c3b
João Sereia <joao.lo.sereia@gmail.com>
2025-09-24 23:03:25 -0300

    chore(fvm): update the version of flutter and dart

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,9 +1,9 @@
 export 'widgets/blur_widget.dart';
 export 'widgets/container_widget.dart';
+export 'widgets/device_radar_widget.dart';
 export 'widgets/divider_widget.dart';
 export 'widgets/icon_button_widget.dart';
 export 'widgets/icon_widget.dart';
 export 'widgets/image_widget.dart';
-export 'widgets/device_radar_widget.dart';
 export 'widgets/text_widget.dart';
 export 'widgets/touchable_widget.dart';
